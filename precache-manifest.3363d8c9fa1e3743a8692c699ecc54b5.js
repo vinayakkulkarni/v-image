22ee40dcc9990fa50c388aef9567239c17166284
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-04-11 22:41:39 +0530

    feat: update new build

---
+++ precache-manifest.3363d8c9fa1e3743a8692c699ecc54b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbfcd176a601a0abab6a35b5a04451e",
+    "url": "index.html"
+  },
+  {
+    "revision": "2538e1e6e19901b7f687254883789183",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "88cb390cf28e0edd947e",
+    "url": "static/css/GithubIcon.b8bfa79b.css"
+  },
+  {
+    "revision": "e15f38c53a953b4940c6",
+    "url": "static/css/app.de4d14f1.css"
+  },
+  {
+    "revision": "88cb390cf28e0edd947e",
+    "url": "static/js/GithubIcon.57b65e7f.js"
+  },
+  {
+    "revision": "e15f38c53a953b4940c6",
+    "url": "static/js/app.a5800abd.js"
+  },
+  {
+    "revision": "64e6c740d8d284130958",
+    "url": "static/js/chunk-vendors.2cce7fe7.js"
+  }
+]);
