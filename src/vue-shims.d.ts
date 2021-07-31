06805e4ce27e08d59f31de98227ce94d7c0c50ab
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-07-31 20:08:16 +0530

    chore: update shims file

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,5 +1,8 @@
 declare module '*.vue' {
   import { defineComponent } from '@vue/composition-api';
-  const component: ReturnType<typeof defineComponent>;
+  import Vue, { VueConstructor } from 'vue';
+  const component: ReturnType<typeof defineComponent> & {
+    install(app: VueConstructor<Vue>): void;
+  };
   export default component;
 }
